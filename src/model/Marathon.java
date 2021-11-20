772e29b29ad799834b66626339f82f905474a9d9
Juan Camilo <juancaram21@gmail.com>
2021-11-20 18:11:17 -0500

    Programming Marathon V1

--- src/model/Marathon.java
+++ src/model/Marathon.java
@@ -14,33 +14,91 @@ public class Marathon {
 		if(root == null) {
 			root = p;
 		} else {
-			boolean sentinel = false;
-			Programmer c = root;
-			
-			while(sentinel == true) {
-				
-				if(c.compareTo(p) < 0) {
-					if(c.getLeft() == null) {
-						c.setLeft(p);
-						sentinel = true;
-					} else {
-						c = c.getLeft();
-					}
+			addProgrammer(p, root);
+		}
+	}
+	
+	private void addProgrammer(Programmer p, Programmer c) {
+		boolean sentinel = false;
+		
+		while(sentinel != true) {
+			if(p.compareTo(c) < 0) {
+				if(c.getLeft() == null) {
+					c.setLeft(p);
+					sentinel = true;
 				} else {
-					if(c.getRight() == null) {
-						c.setRight(p);
-						sentinel = true;
-					} else {
-						c = c.getRight();
-					}
+					c = c.getLeft();
+				}
+			} else {
+				if(c.getRight() == null) {
+					c.setRight(p);
+					sentinel = true;
+				} else {
+					c = c.getRight();
 				}
 			}
 		}
 	}
-
+	
+	public int getHeight() {
+		if(root == null) {
+			return 0;
+		} else {
+			return getHeight(root);
+		}
+	}
+	
+	//Return the Binary Tree Height
+	private int getHeight(Programmer c) {	
+		if(c.getLeft() != null || c.getRight() != null) {
+			if(c.getLeft() != null && c.getRight() != null) {
+				int left = getHeight(c.getLeft()) + 1; //This 1 is for the root
+				int right = getHeight(c.getRight()) + 1; //This 1 is for the root
+				
+				return returnMax(left, right);
+			} else if(c.getLeft() != null) {
+				return getHeight(c.getLeft()) + 1;
+			} else {
+				return getHeight(c.getRight()) + 1;
+			}
+		} else {
+			return 1;
+		}
+	}
+	
+	
+	private int returnMax(int numb1, int numb2) {
+		if(numb1 > numb2) {
+			return numb1;
+		} else {
+			return numb2;
+		}
+	}
 	
 	public String print() {
-		return "Holi";
+		if(root == null) {
+			return "There are not registered programmers";
+		} else {
+			return print(root);
+		}
+	}
+	
+	private String print(Programmer c) {
+		String s = "";
+		
+		if(c != null) {
+			if(c.getLeft() != null) {
+				s += print(c.getLeft());
+			}
+			
+			s += c.getName() + "\n";
+			
+			if(c.getRight() != null){
+				s += print(c.getRight());
+			}
+		}
+
+		return s;
 	}
 	
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -11,8 +11,15 @@ public class Main {
 		
 		m.addProgrammer("Juan Camilo Ramirez Tabares");
 		m.addProgrammer("Samuel Ramirez Tabares");
+		m.addProgrammer("Jesus David Rodriguez Burbano");
 		m.addProgrammer("Pulguita");
+		m.addProgrammer("Juan Felipe Castillo Gomez");
+		m.addProgrammer("Andrea Jaramillo Parra");
+		m.addProgrammer("Maria Jose Quintana Urrego");
+		m.addProgrammer("Betty Infante Ponce");
 		
+		System.out.println("Programmers List:\n" + m.print());
+		System.out.println("Binary Tree Height: " + m.getHeight());
 	}
 	
 }
